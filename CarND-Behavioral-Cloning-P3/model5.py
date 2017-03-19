8d192d7933aeec93ee0981f4501d93f4cbd35293
Andreas <andreas28@users.noreply.github.com>
2017-03-19 19:44:54 +0100

    modified to reduce center images

--- CarND-Behavioral-Cloning-P3/model5.py
+++ CarND-Behavioral-Cloning-P3/model5.py
@@ -11,8 +11,8 @@ import numpy as np
 from sklearn.utils import shuffle
 
 
-batch_size = 64#32
-nb_epoch = 1#10
+batch_size = 16#32
+nb_epoch = 5#10
 crop_bottom = 25
 crop_top = 70#75 #Crop less to see further into curves
 steering_offset = 0.10
@@ -41,7 +41,7 @@ def generator(samples, batch_size=32):
             angles = []
             for batch_sample in batch_samples:
 
-                camera = np.random.choice(['center', 'left', 'right'])
+                camera = np.random.choice(['center', 'left', 'right'], p=[0.1,0.45,0.45])
                 flip = np.random.choice(['flip', 'noflip'])
                 filename = ""
                 angle = float(batch_sample[3])
@@ -55,9 +55,7 @@ def generator(samples, batch_size=32):
                     filename = './data/data/IMG/' + batch_sample[2].split('/')[-1]
                     angle = (angle * steering_rate) - steering_offset
 
-                # Read and crop Image
                 image = cv2.imread(filename)
-                #image = image[crop_top:image.shape[0]-crop_bottom, 0:image.shape[1]]
 
                 # Flip
                 if flip == "flip":
@@ -71,12 +69,14 @@ def generator(samples, batch_size=32):
                 X_train = np.array(images)
                 y_train = np.array(angles)
 
-                if False:
+                if True:
                     print ("\n\n")
                     print (flip)
                     print (camera)
                     print(float(batch_sample[3]))
                     print (angle)
+                    print ("Angles")
+                    print (angles)
                     cv2.imshow("test", image)
                     cv2.waitKey(-1)
 
@@ -86,28 +86,6 @@ def generator(samples, batch_size=32):
 train_generator = generator(train_samples, batch_size=batch_size)
 validation_generator = generator(validation_samples, batch_size=batch_size)
 
-
-# measurements = []
-# images = []
-# for line in lines:
-#     for i in range(3):              #all 3 images
-#         source_path = line[i]
-#         filename = source_path.split('/')[-1]
-#         current_path = './data/data/IMG/' + filename
-#         image = cv2.imread(current_path)
-#         images.append(image)
-#         measurement = float(line[3])
-#         measurements.append(measurement)
-#
-# augmented_images, augmented_measurements = [], []
-# for image,measurement in zip(images, measurements):
-#     augmented_images.append(image)
-#     augmented_measurements.append(measurement)
-#     augmented_images.append(cv2.flip(image,1))
-#     augmented_measurements.append(measurement*-1.0)
-#
-# X_train = np.array(augmented_images)
-# y_train = np.array(augmented_measurements)
 
 
 #Nvidia
@@ -120,7 +98,6 @@ model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
 model.add(Convolution2D(64,5,5, subsample=(1,1), activation="relu"))
 model.add(Convolution2D(64,5,5, subsample=(1,1), activation="relu"))
 model.add(Flatten())
-model.add(Dense(1164))
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
@@ -128,9 +105,9 @@ model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator,
-                    samples_per_epoch=3000,#len(train_samples)*6,
+                    samples_per_epoch=len(train_samples)*6,
                     validation_data=validation_generator,
-                    nb_val_samples=500,#len(validation_samples)*6,
+                    nb_val_samples=len(validation_samples)*6,
                     nb_epoch=nb_epoch,
                     verbose=1)
 
