78a899935af123e20b830042d0a958b2c77bcae9
Andreas <andreas28@users.noreply.github.com>
2017-03-19 22:19:33 +0100

    Model with multiple data and only left right

--- CarND-Behavioral-Cloning-P3/model5.py
+++ CarND-Behavioral-Cloning-P3/model5.py
@@ -11,16 +11,17 @@ import numpy as np
 from sklearn.utils import shuffle
 
 
-batch_size = 16#32
-nb_epoch = 5#10
+batch_size = 32#32
+nb_epoch = 3#10
 crop_bottom = 25
 crop_top = 70#75 #Crop less to see further into curves
-steering_offset = 0.10
+steering_offset = 0.2
 steering_rate = 1.1
+samples_factor = 6
 
 # Read in lines from files
 lines = []
-with open('./data/data/driving_log.csv') as csvfile:
+with open('./data/data_combined/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     next(reader, None)  # skip the headers
     for line in reader:
@@ -41,18 +42,18 @@ def generator(samples, batch_size=32):
             angles = []
             for batch_sample in batch_samples:
 
-                camera = np.random.choice(['center', 'left', 'right'], p=[0.1,0.45,0.45])
+                camera = np.random.choice(['center', 'left', 'right'], p=[0.0,0.5,0.5])
                 flip = np.random.choice(['flip', 'noflip'])
                 filename = ""
                 angle = float(batch_sample[3])
 
                 if camera == "center":
-                    filename = './data/data/IMG/' + batch_sample[0].split('/')[-1]
+                    filename = '.'+batch_sample[0].split('CarND-Behavioral-Cloning-P3')[1]
                 elif camera == "left":
-                    filename = './data/data/IMG/' + batch_sample[1].split('/')[-1]
+                    filename = '.'+batch_sample[1].split('CarND-Behavioral-Cloning-P3')[1]
                     angle = (angle * steering_rate) + steering_offset
                 elif camera == "right":
-                    filename = './data/data/IMG/' + batch_sample[2].split('/')[-1]
+                    filename = '.'+batch_sample[2].split('CarND-Behavioral-Cloning-P3')[1]
                     angle = (angle * steering_rate) - steering_offset
 
                 image = cv2.imread(filename)
@@ -69,8 +70,9 @@ def generator(samples, batch_size=32):
                 X_train = np.array(images)
                 y_train = np.array(angles)
 
-                if True:
+                if False:
                     print ("\n\n")
+                    print (filename)
                     print (flip)
                     print (camera)
                     print(float(batch_sample[3]))
@@ -93,21 +95,24 @@ model = Sequential()
 model.add(Cropping2D(cropping=((crop_top,crop_bottom),(0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: x / 255.0 - 0.5))
 model.add(Convolution2D(24,5,5, subsample=(1,1), activation="relu"))
-model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36,5,5, subsample=(1,1), activation="relu"))
+model.add(MaxPooling2D())
 model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
 model.add(Convolution2D(64,5,5, subsample=(1,1), activation="relu"))
 model.add(Convolution2D(64,5,5, subsample=(1,1), activation="relu"))
+model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
+
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator,
-                    samples_per_epoch=len(train_samples)*6,
+                    samples_per_epoch=len(train_samples)*samples_factor,
                     validation_data=validation_generator,
-                    nb_val_samples=len(validation_samples)*6,
+                    nb_val_samples=len(validation_samples)*samples_factor,
                     nb_epoch=nb_epoch,
                     verbose=1)
 
