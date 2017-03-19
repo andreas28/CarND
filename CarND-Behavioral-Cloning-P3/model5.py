6806e78d4234fb8cf947c27346b48b034e67d5dc
Andreas <andreas28@users.noreply.github.com>
2017-03-19 23:51:53 +0100

    Model with multiple data and only left right

--- CarND-Behavioral-Cloning-P3/model5.py
+++ CarND-Behavioral-Cloning-P3/model5.py
@@ -12,12 +12,12 @@ from sklearn.utils import shuffle
 
 
 batch_size = 32#32
-nb_epoch = 3#10
+nb_epoch = 5#10
 crop_bottom = 25
-crop_top = 70#75 #Crop less to see further into curves
-steering_offset = 0.2
-steering_rate = 1.1
-samples_factor = 6
+crop_top = 75#75 #Crop less to see further into curves
+steering_offset = 0.2#0.2
+steering_rate = 1.0#1.0
+samples_factor = 5
 
 # Read in lines from files
 lines = []
@@ -31,7 +31,6 @@ from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
 
-
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1:
@@ -42,7 +41,7 @@ def generator(samples, batch_size=32):
             angles = []
             for batch_sample in batch_samples:
 
-                camera = np.random.choice(['center', 'left', 'right'], p=[0.0,0.5,0.5])
+                camera = np.random.choice(['center', 'left', 'right'], p=[0.1,0.45,0.45])
                 flip = np.random.choice(['flip', 'noflip'])
                 filename = ""
                 angle = float(batch_sample[3])
@@ -94,13 +93,13 @@ validation_generator = generator(validation_samples, batch_size=batch_size)
 model = Sequential()
 model.add(Cropping2D(cropping=((crop_top,crop_bottom),(0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: x / 255.0 - 0.5))
-model.add(Convolution2D(24,5,5, subsample=(1,1), activation="relu"))
-model.add(Convolution2D(36,5,5, subsample=(1,1), activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
-model.add(Convolution2D(64,5,5, subsample=(1,1), activation="relu"))
-model.add(Convolution2D(64,5,5, subsample=(1,1), activation="relu"))
-model.add(MaxPooling2D())
+model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
+#model.add(MaxPooling2D())
+model.add(Convolution2D(48,5,5, subsample=(1,1), activation="relu"))
+model.add(Convolution2D(64,3,3, subsample=(1,1), activation="relu"))
+model.add(Convolution2D(64,3,3, subsample=(1,1), activation="relu"))
+#model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
