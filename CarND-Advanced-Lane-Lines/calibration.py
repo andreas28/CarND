012ccc67e643b02d72c9e9a0c7167f5b9b270a8d
Andreas <andreas28@users.noreply.github.com>
2017-04-02 20:23:14 +0200

    thresholding and structure

--- CarND-Advanced-Lane-Lines/calibration.py
+++ CarND-Advanced-Lane-Lines/calibration.py
@@ -5,65 +5,87 @@ import pickle
 #import matplotlib.pyplot as plt
 #matplotlib qt
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*9,3), np.float32)
-objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d points in real world space
-imgpoints = [] # 2d points in image plane.
-
-# Make a list of calibration images
-images = glob.glob('camera_cal/calibration*.jpg')
-
-# Step through the list and search for chessboard corners
-for idx, fname in enumerate(images):
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Find the chessboard corners
-    ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
-
-    # If found, add object points, image points
-    if ret == True:
-        objpoints.append(objp)
-        imgpoints.append(corners)
-
-        # Draw and display the corners
-        cv2.drawChessboardCorners(img, (9,6), corners, ret)
-        #write_name = 'corners_found'+str(idx)+'.jpg'
-        #cv2.imwrite(write_name, img)
-        cv2.imshow('img', img)
-        cv2.waitKey(10)
-
-cv2.destroyAllWindows()
-
-# Test undistortion on an image
-img = cv2.imread('camera_cal/calibration1.jpg')
-img_size = (img.shape[1], img.shape[0])
-
-# Do camera calibration given object points and image points
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
-
-# Undistort image
-dst = cv2.undistort(img, mtx, dist, None, mtx)
-
-# Visualize before after
-show = np.hstack((img, dst))
-cv2.imshow('Before After', show)
-cv2.waitKey(10)
-cv2.imwrite('distorted_undistorted1.jpg',show)
+# Load camera matrix and distortion parameters from file
+def load_calib_data(filename):
+    calib_data_file = open(filename, "rb")
+    calib_data = pickle.load(calib_data_file)
+    calib_mtx = calib_data["mtx"]
+    calib_dist = calib_data["dist"]
+    calib_data_file.close()
+    return calib_mtx, calib_dist
 
 
+# Calibrate camera and save camera matrix and distortion parameters to file
+def main():
 
-cv2.destroyAllWindows()
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((6*9,3), np.float32)
+    objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)
 
-print (mtx)
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d points in real world space
+    imgpoints = [] # 2d points in image plane.
 
-print (dist)
+    # Make a list of calibration images
+    images = glob.glob('camera_cal/calibration*.jpg')
 
-# Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
-dist_pickle = {}
-dist_pickle["mtx"] = mtx
-dist_pickle["dist"] = dist
-pickle.dump( dist_pickle, open( "calib_data.p", "wb" ) )
+    # Step through the list and search for chessboard corners
+    for idx, fname in enumerate(images):
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Find the chessboard corners
+        ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
+
+        # If found, add object points, image points
+        if ret == True:
+            objpoints.append(objp)
+            imgpoints.append(corners)
+
+            # Draw and display the corners
+            cv2.drawChessboardCorners(img, (9,6), corners, ret)
+            #write_name = 'corners_found'+str(idx)+'.jpg'
+            #cv2.imwrite(write_name, img)
+            cv2.imshow('img', img)
+            cv2.waitKey(10)
+
+    cv2.destroyAllWindows()
+
+    # Test undistortion on an image
+    img = cv2.imread('camera_cal/calibration1.jpg')
+    img_size = (img.shape[1], img.shape[0])
+
+    # Do camera calibration given object points and image points
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
+
+    # Undistort image
+    dst = cv2.undistort(img, mtx, dist, None, mtx)
+
+    # Visualize before after
+    show = np.hstack((img, dst))
+    cv2.imshow('Before After', show)
+    cv2.waitKey(10)
+    cv2.imwrite('output_images/distorted_undistorted.jpg',show)
+
+    # Undistort example image
+    img = cv2.imread("test_images/straight_lines1.jpg")
+    dst = cv2.undistort(img, mtx, dist, None, mtx)
+    show = np.hstack((img, dst))
+    cv2.imshow('Before After', show)
+    cv2.waitKey(10)
+    cv2.imwrite('output_images/distorted_undistorted2.jpg',show)
+
+    cv2.destroyAllWindows()
+
+    print (mtx)
+    print (dist)
+
+    # Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
+    dist_pickle = {}
+    dist_pickle["mtx"] = mtx
+    dist_pickle["dist"] = dist
+    pickle.dump( dist_pickle, open( "calib_data.p", "wb" ) )
+
+if __name__ == '__main__':
+    main()
--- CarND-Advanced-Lane-Lines/thresholding.py
+++ CarND-Advanced-Lane-Lines/thresholding.py
@@ -1,16 +1,32 @@
 from calibration import load_calib_data
 import glob
 import cv2
+import numpy as np
 
 
 def thresholding( image ):
 
-    img_Lab = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
-    #img_Lab = img_Lab[:,:,2]
+    sobel_kernel = 3
+
+    img_HLS = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
+    img_HLS_S = img_HLS[:,:,2]
+    ret, img_HLS_S = cv2.threshold(img_HLS_S, 170, 255, cv2.THRESH_BINARY)
 
 
 
-    return img_Lab
+    sobelx = cv2.Sobel(img_HLS[:,:,1], cv2.CV_64F, 1, 0, ksize=sobel_kernel) # Take the derivative in x
+    sobely = cv2.Sobel(img_HLS[:,:,1], cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= 20) & (scaled_sobel <= 100)] = 1
+    sxbinary = np.uint8(255*sxbinary/np.max(sxbinary))
+
+
+
+    return img_HLS_S, scaled_sobel
 
 
 def main():
@@ -24,12 +40,12 @@ def main():
     for img_file in images:
         img = cv2.imread(img_file)
         img_undistorted = cv2.undistort(img, calib_mtx, calib_dist, None, calib_mtx)
-        img = thresholding(img_undistorted)
+        img_HLS_S, sxbinary = thresholding(img_undistorted)
 
         cv2.imshow('img_undist', img_undistorted)
-        cv2.imshow('img0', img[:,:,0])
-        cv2.imshow('img1', img[:,:,1])
-        cv2.imshow('img2', img[:,:,2])
+        cv2.imshow('img_HLS_S', img_HLS_S)
+        cv2.imshow('sxbinary', sxbinary)
+
         cv2.waitKey(-1)
 
     cv2.destroyAllWindows()
