88d8c5cdb6364b946376930a99d392256da78b51
Andreas <andreas28@users.noreply.github.com>
2017-04-29 21:47:18 +0200

    first

---
+++ CarND-Vehicle-Detection-master/main.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+from moviepy.editor import VideoFileClip
+from classifier import load_classifier
+from lesson_functions import *
+
+
+
+def main():
+    print ("main")
+
+
+
+
+if __name__ == '__main__':
+    main()
