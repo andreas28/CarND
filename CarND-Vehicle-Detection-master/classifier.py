7a06f8e86bd8a1bfdf598ef17e02e07e0aed9a7d
Andreas <andreas28@users.noreply.github.com>
2017-04-29 22:24:51 +0200

    :

--- CarND-Vehicle-Detection-master/classifier.py
+++ CarND-Vehicle-Detection-master/classifier.py
@@ -8,6 +8,7 @@ from skimage.feature import hog
 from lesson_functions import *
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+import random
 
 
 # Define a function to extract features from a single image window
@@ -97,13 +98,18 @@ def load_classifier():
     return 0
 
 
-def get_images(object='cars'):
+def get_images(object='cars', number=None):
     if object=='cars':
         images = glob.glob("/home/andreas/work/CarND/CarND-Vehicle-Detection-master/data/vehicles/**/*.png", recursive=True)
     elif object=='non_cars':
         images = glob.glob("/home/andreas/work/CarND/CarND-Vehicle-Detection-master/data/non-vehicles/**/*.png", recursive=True)
     else:
         raise ValueError('get_images parameter must be cars or non_cars')
+
+    if number==None or number>len(images):
+        return images
+    else:
+        images = random.sample(images, number)
     return images
 
 def main():
@@ -204,7 +210,34 @@ def main():
 
 if __name__ == '__main__':
 
-    cars = get_images("cars")
-    non_cars = get_images("non_cars")
+    cars = get_images("cars", 5)
+    notcars = get_images("non_cars", 5)
+
+
+    color_space = 'RGB' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+    orient = 9  # HOG orientations
+    pix_per_cell = 8 # HOG pixels per cell
+    cell_per_block = 2 # HOG cells per block
+    hog_channel = 0 # Can be 0, 1, 2, or "ALL"
+    spatial_size = (16, 16) # Spatial binning dimensions
+    hist_bins = 16    # Number of histogram bins
+    spatial_feat = True # Spatial features on or off
+    hist_feat = True # Histogram features on or off
+    hog_feat = True # HOG features on or off
+    y_start_stop = [None, None] # Min and max in y to search in slide_window()
+
+    car_features = extract_features(cars, color_space=color_space,
+                        spatial_size=spatial_size, hist_bins=hist_bins,
+                        orient=orient, pix_per_cell=pix_per_cell,
+                        cell_per_block=cell_per_block,
+                        hog_channel=hog_channel, spatial_feat=spatial_feat,
+                        hist_feat=hist_feat, hog_feat=hog_feat)
+    notcar_features = extract_features(notcars, color_space=color_space,
+                        spatial_size=spatial_size, hist_bins=hist_bins,
+                        orient=orient, pix_per_cell=pix_per_cell,
+                        cell_per_block=cell_per_block,
+                        hog_channel=hog_channel, spatial_feat=spatial_feat,
+                        hist_feat=hist_feat, hog_feat=hog_feat)
+
     #main()
 
